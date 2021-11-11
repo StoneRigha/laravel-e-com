0e7b76e7f9c531f835dba5362574cafb9d023503
StoneRiga <stoneriga@gmail.com>
2021-11-11 12:16:10 +0300

    header, login and footer done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue fixed-bottom">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2021 Copyright:
+      <a href="https://www.stonified.wordpress.com"> Stonified</a>
+    </div>
+    <!-- Copyright -->
+  
+  </footer>
+  <!-- Footer -->

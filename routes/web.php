79f2f3f42601657740654570fa8adc01e5f95d70
StoneRiga <stoneriga@gmail.com>
2021-11-16 09:34:25 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,9 @@ use Symfony\Component\Console\Helper\ProgressIndicator;
 Route::get('/login', function () {
     return view('login');
 });
-Route::get('/header', function () {
-    return view ('header');
+Route::get('/logout', function () {
+    Session::forget('user');
+    return redirect ('login');
 });
 Route::post('/login', [UserController::class, 'login']);
 Route::get('/', [ProductController::class, 'index']);
